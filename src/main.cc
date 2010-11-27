5adb3f4f4f7d3567b8a0094c100750cc2958dd3d
Brandon Fosdick <bfoz@bfoz.net>
2010-11-26 18:05:46 -0800

    Add a skeleton main file
    First compilable and runnable version. It doesn't do anything.

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+/*  HID Explorer
+    A cross-platform interface for discovering and inspecting HID devices
+
+    Copyright 2010 Brandon Fosdick <bfoz@bfoz.net> (BSD License)
+*/
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    return app.exec();
+}
