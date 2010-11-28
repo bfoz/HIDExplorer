44064fe94d9fd8ebabbff01f994b1ef335d1c76d
Brandon Fosdick <bfoz@bfoz.net>
2010-11-27 18:47:02 -0800

    Display a simple list view of all available devices
    Only displays the device's product string

--- src/main.cc
+++ src/main.cc
@@ -5,10 +5,25 @@
 */
 
 #include <QApplication>
+#include <QListWidget>
+
+#include <hid.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    HID::device_list devices = HID::find();
+
+    QListWidget* listWidget = new QListWidget;
+    HID::device_list::iterator i = devices.begin();
+    for(; i != devices.end(); ++i)
+    {
+	QListWidgetItem* newItem = new QListWidgetItem((*i)->product().c_str());
+	listWidget->addItem(newItem);
+
+	listWidget->show();
+    }
 
     return app.exec();
 }
