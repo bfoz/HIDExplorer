41d06e6e1c381208568bb8ca7e02d4ecf13d1996
Brandon Fosdick <bfoz@bfoz.net>
2011-04-17 22:34:45 -0700

    Fix some layout issues
    Force the layout manager to expand the top level widget horizontally instead of shrinking the device listview.

--- src/main.cc
+++ src/main.cc
@@ -38,9 +38,17 @@ int main(int argc, char *argv[])
     tabs->addTab(delegate->detailWidget, "Info");
     tabs->addTab(delegate->elementsWidget, "Elements");
 
+    /* Set a larger stretch on the QTabWidget so it will expand to match the
+	selected device. But, the layout manager will prefer to take space from
+	the list widget instead of expanding the main window. So, set the list
+	widget's minimum width to the width of its sizeHint to force the window
+	to be expanded.
+    */
     QHBoxLayout* hbox = new QHBoxLayout();
     hbox->addWidget(delegate->listWidget);
-    hbox->addWidget(tabs);
+    hbox->addWidget(tabs, 1);
+    const QSize s = delegate->listWidget->sizeHint();
+    delegate->listWidget->setMinimumWidth(s.width());
 
     QWidget* widget = new QWidget;
     widget->setLayout(hbox);
